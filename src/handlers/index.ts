a073df606e5c09dae72a9df05703f78be44a3aa5
SPLATPLAYS <splatplaysisgood@gmail.com>
2026-02-18 17:42:51 +0800

    Added lzh and lha support

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -26,6 +26,7 @@ import flptojsonHandler from "./flptojson.ts";
 import floHandler from "./flo.ts";
 import batToExeHandler from "./batToExe.ts";
 import textEncodingHandler from "./textEncoding.ts";
+import lzhHandler from "./lzh.ts";
 
 const handlers: FormatHandler[] = [];
 try { handlers.push(new svgTraceHandler()) } catch (_) { };
@@ -56,5 +57,6 @@ try { handlers.push(new flptojsonHandler()) } catch (_) { };
 try { handlers.push(new floHandler()) } catch (_) { };
 try { handlers.push(new batToExeHandler()) } catch (_) { };
 try { handlers.push(new textEncodingHandler()) } catch (_) { };
+try { handlers.push(new lzhHandler()) } catch (_) { };
 
 export default handlers;
