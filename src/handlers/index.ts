1e66437e9c6c47b8240f60ba139b50695e78eb76
Pixel <pixelsworkemail@gmail.com>
2026-02-17 20:44:23 -0600

    Added Wld handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -26,6 +26,7 @@ import flptojsonHandler from "./flptojson.ts";
 import floHandler from "./flo.ts";
 import batToExeHandler from "./batToExe.ts";
 import textEncodingHandler from "./textEncoding.ts";
+import WldHandler from "./WldHandler.js";
 
 const handlers: FormatHandler[] = [];
 try { handlers.push(new svgTraceHandler()) } catch (_) { };
@@ -56,5 +57,6 @@ try { handlers.push(new flptojsonHandler()) } catch (_) { };
 try { handlers.push(new floHandler()) } catch (_) { };
 try { handlers.push(new batToExeHandler()) } catch (_) { };
 try { handlers.push(new textEncodingHandler()) } catch (_) { };
+try { handlers.push(WldHandler) } catch (_) { };
 
 export default handlers;
