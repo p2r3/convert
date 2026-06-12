4099083b869436246fbbca375cb82003b3caf749
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2026-06-13 00:35:38 +0300

    Reverted the from png to kra changes, it is possible to convert and if someone wants to do it here is the file in the krita codebase (krita/libs/ui/kis_png_converter.cpp ) which will help you

--- src/handlers/kra.ts
+++ src/handlers/kra.ts
@@ -6,86 +6,6 @@ class kraHandler implements FormatHandler {
   public name: string = "kra";
   public supportedFormats?: FileFormat[];
   public ready: boolean = false;
-  buildmaindocXml = (
-    width: number,
-    height: number,
-    xres: number,
-    yres: number,
-    description: string,
-    colorspacename: string,
-    backgroundColor?: string,
-  ): string => {
-    const layers = [
-      { name: "Paint Layer 1", uuid: crypto.randomUUID() },
-      { name: "Background", uuid: crypto.randomUUID() },
-    ];
-    const maindocXml = `<DOC xmlns="http://www.calligra.org/DTD/krita" editor="Krita" kritaVersion="6.0.1" syntaxVersion="2.0">
-<IMAGE colorspacename="${colorspacename}" description="${description}" height="${height}" mime="application/x-kra" name="Test" profile="sRGB-elle-V2-srgbtrc.icc" width="${width}" x-res="${xres}" y-res="${yres}">
-<layers>
-<layer channelflags="" channellockflags="" collapsed="0" colorlabel="0" colorspacename="RGBA" compositeop="normal" filename="layer2" intimeline="1" locked="0" name="${layers[0].name}" nodetype="paintlayer" onionskin="0" opacity="255" selected="true" uuid="{${layers[0].uuid}}" visible="1" x="0" y="0"/>
-<layer channelflags="" channellockflags="" collapsed="0" colorlabel="0" colorspacename="RGBA" compositeop="normal" filename="layer3" intimeline="1" locked="1" name="${layers[1].name}" nodetype="paintlayer" onionskin="0" opacity="255" uuid="{${layers[1].uuid}}" visible="1" x="0" y="0"/>
-</layers>
-<ProjectionBackgroundColor ColorData="AAAAAA=="/>
-<GlobalAssistantsColor SimpleColorData="176,176,176,255"/>
-<MirrorAxis>
-<mirrorHorizontal type="value" value="0"/>
-<mirrorVertical type="value" value="0"/>
-<lockHorizontal type="value" value="0"/>
-<lockVertical type="value" value="0"/>
-<hideHorizontalDecoration type="value" value="0"/>
-<hideVerticalDecoration type="value" value="0"/>
-<handleSize type="value" value="32"/>
-<horizontalHandlePosition type="value" value="64"/>
-<verticalHandlePosition type="value" value="64"/>
-<axisPosition type="pointf" x="256" y="256"/>
-</MirrorAxis>
-<ColorHistory>
-<RGB b="0.505882382392883" g="0.0588235296308994" r="0" space="sRGB-elle-V2-srgbtrc.icc"/>
-<RGB b="0" g="0.505882382392883" r="0.243137255311012" space="sRGB-elle-V2-srgbtrc.icc"/>
-<RGB b="0" g="0" r="0.505882382392883" space="sRGB-elle-V2-srgbtrc.icc"/>
-<RGB b="0" g="0" r="0" space="sRGB"/>
-</ColorHistory>
-<Palettes/>
-<resources/>
-<animation>
-<framerate type="value" value="24"/>
-<range from="0" to="100" type="timerange"/>
-<currentTime type="value" value="0"/>
-</animation>
-</IMAGE>
-</DOC>`;
-    return maindocXml;
-  };
-
-  buildDocumentInfoXml = (title: string, currentDate: string): string => `
-<document-info xmlns="http://www.calligra.org/DTD/document-info">
-<about>
-<title>${title}</title>
-<description/>
-<subject/>
-<abstract>
-<![CDATA[ ]]>
-</abstract>
-<keyword/>
-<initial-creator>Unknown</initial-creator>
-<editing-cycles>1</editing-cycles>
-<editing-time/>
-<date>${currentDate}</date>
-<creation-date>${currentDate}</creation-date>
-<language/>
-<license/>
-</about>
-<author>
-<full-name/>
-<creator-first-name/>
-<creator-last-name/>
-<initial/>
-<author-title/>
-<position/>
-<company/>
-</author>
-</document-info>
-`;
 
   async init() {
     this.supportedFormats = [
@@ -141,30 +61,7 @@ class kraHandler implements FormatHandler {
         });
       }
     }
-    if (inputFormat.format == "png" && outputFormat.format == "kra") {
-      for (const inputFile of inputFiles) {
-        // steps for conversion
-        // 1. Create a new zip file [done]
-        // 2. Add the PNG file to the zip with the name "mergedimage.png" [done]
-        // 3. scale down the png to 256x256 and add it to the zip with the name "preview.png"
-        // 4. add a mimetype file with the content "application/x-krita"
-        // 5. maindoc.xml
-        // 6. documentinfo.xml [done]
-        // 7. layer data in imagename/layers/layerdatafile
-        // 8. add the zip file to the outputFiles array with the name of the input file but with the .kra extension
 
-        const zip = new JSZip();
-        zip.file("mergedimage.png", inputFile.bytes);
-        zip.file("mimetype", "application/x-krita");
-
-        const currentDate = new Date().toISOString();
-        const documentInfoXml = this.buildDocumentInfoXml(
-          inputFile.name,
-          currentDate,
-        );
-        zip.file("documentinfo.xml", documentInfoXml);
-      }
-    }
     return outputFiles;
   }
 }
